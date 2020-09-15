db46b1c86624bd8704e8a327c32d6c01f00e10b0
AtharShihab <atharshihabc2@gmail.com>
2020-09-15 15:17:17 +0600

    Exercise 3c: Added Movie properties, Movie Genre and Showed Movie details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,22 +10,37 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
-        public List<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            var movies = new List<Movie>()
-            {
-                new Movie { Id = 1, Name = "Shrek"},
-                new Movie { Id = 2, Name = "Wall-E"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-            return movies;
+            if(movie == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(movie);
         }
     }
 }
